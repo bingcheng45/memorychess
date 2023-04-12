1b65b4290755975f07eb9902fb8b9c1150824959
bingcheng45 <bingcheng45@gmail.com>
2023-04-13 06:32:44 +0800

    configuration

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,13 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react({
-    include: ["**/*.js", "**/*.jsx", "**/*.ts", "**/*.tsx", "**/*.css"],
-  })],
+  plugins: [
+    react({
+      include: ['**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx', '**/*.css'],
+    }),
+  ],
+  build: {
+    outDir: 'dist',
+  },
+  base: '/memorychess/',
 })
