a487c15786798666f3fcd407f6e4802aa30f5858
bingcheng45 <bingcheng45@gmail.com>
2023-04-15 21:37:29 +0800

    fix typo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ import CustomChessboard from "../components/CustomChessboard";
 function Home() {
   return (
     <div className="z-0">
-      <h1 class="font-extrabold text-transparent text-center text-2xl bg-clip-text bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90%">
+      <h1 className="font-extrabold text-transparent text-center text-2xl bg-clip-text bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90%">
         Memorize and fill in the chessboard!
       </h1>
       {/* <div style={{ width: "70%", margin: "0 auto" }}>
