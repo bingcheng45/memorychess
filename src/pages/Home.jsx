7635e8fa78bdbf1d066353150c3c5984fce69284
bingcheng45 <bingcheng45@gmail.com>
2023-12-24 18:04:46 +0800

    add: start button

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,9 +4,12 @@ import React, { useState, useEffect } from "react";
 import { Chessboard } from "react-chessboard";
 import CustomChessboard from "../components/CustomChessboard";
 import { detectDevice } from "../utils/DetectDevice";
+import '../assets/css/Home.css';
+
 
 function Home() {
   const [isMobile, setIsMobile] = useState(false);
+  const [gameStarted, setGameStarted] = useState(false);
   useEffect(() => {
     const userAgent = navigator.userAgent || navigator.vendor;
     if (
@@ -19,15 +22,26 @@ function Home() {
       setIsMobile(false);
     }
   }, [isMobile]);
+
+  const startGame = () => {
+    setGameStarted(true);
+  };
+
   return (
     <div className="z-0">
       <h1 className="font-extrabold text-transparent text-center text-2xl bg-clip-text bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90%">
         Memorize and fill in the chessboard!
       </h1>
-      {/* <div style={{ width: "70%", margin: "0 auto" }}>
-        <Chessboard position={{}} width={600} />
-      </div> */}
-      <CustomChessboard />
+      
+      {/* Container for the button and chessboard */}
+      <div className="chessboard-container">
+        {!gameStarted && (
+          <button onClick={startGame} className="start-game-button">
+            Start Game
+          </button>
+        )}
+        {gameStarted && <CustomChessboard />}
+      </div>
 
       <div className="text-center">
         <br />
